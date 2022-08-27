1d15f9fbc08fb3fced4cce1542474fc3f0470992
Facus2020 <facumayorga@outlook.com>
2022-08-26 21:46:44 -0300

    Agregado Guardar Formulario

--- diario/views.py
+++ diario/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 import datetime
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -15,5 +16,15 @@ def post_detail(request, pk):
     return render(request, 'diario/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list',)
+    else:
+        form = PostForm()
     return render(request, 'diario/post_edit.html', {'form': form})
+
